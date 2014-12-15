66d9b0c13fb804248d1345ed2d1a37d072ba5f87
brunosmmm <brunosmmm@gmail.com>
2014-12-14 19:57:20 -0500

    Some work on the instruction memory for loading data from files generated by the assembler

---
+++ instrset.cpp
@@ -0,0 +1,13 @@
+/*
+ * instrset.cpp
+ *
+ *  Created on: 14/12/2014
+ *      Author: bruno
+ */
+
+#include "instrset.h"
+
+ANEMInstruction ANEM_INSTRUCTION_NOP = {ANEM_OPCODE_R,{0,{0,ANEM_FUNC_OR}}};
+
+
+
